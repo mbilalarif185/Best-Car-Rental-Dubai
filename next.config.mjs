b39f845609d4f346d7f9a0038a7c3411a312b140
MUHAMMAD BILAL ARIF <04071913032@student.qau.edu.pk>
2025-04-30 12:16:25 +0500

    Updated cars-list-3/page.tsx

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    eslint: {
-      ignoreDuringBuilds: true, // Disable ESLint checks during build
-    },
-    typescript: {
-      ignoreBuildErrors: process.env.NODE_ENV === 'production', // Ignore TypeScript errors in production
-    },
-    images: {
-      domains: ['bestcarrentaldubai.ae'],
-    },
-  };
-  
-  export default nextConfig;
-  
+  eslint: {
+    ignoreDuringBuilds: true, // Disable ESLint checks during build
+  },
+  typescript: {
+    ignoreBuildErrors: process.env.NODE_ENV === 'production', // Ignore TypeScript errors in production
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'bestcarrentaldubai.ae',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
