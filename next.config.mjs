e11d46e0e11caba0fb2e9e5d430702e0ad39d513
MUHAMMAD BILAL ARIF <04071913032@student.qau.edu.pk>
2025-04-30 10:40:23 +0500

    Disabled ESLint and TypeScript checks for production

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    eslint: {
+      ignoreDuringBuilds: true, // Disable ESLint checks during build
+    },
+    typescript: {
+      ignoreBuildErrors: process.env.NODE_ENV === 'production', // Ignore TypeScript errors in production
+    },
+  };
+  
+  export default nextConfig;
+  
